881a441e63f7937d911b79f93add5c6112e31ee7
Yue Wang <hin@Yue-Wang.local>
2018-12-06 20:46:06 +0000

    Linting clean

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hotel Booking System' });
+    res.render('index', { title: 'Hotel Booking System' });
 });
 
 module.exports = router;
